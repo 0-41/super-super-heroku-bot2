df8679a294ec4325f40a0997e81f578c93a4012c
aziz127182 <32429099+aziz127182@users.noreply.github.com>
2017-10-01 11:38:35 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,25 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('I am ready!');
-});
-
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+   client.on('message', message => {
+     if (message.content === "-help") {
+message.author.send(".Best Commands" + `  **
+ ".General Commands"
+🔧               -ping | سرعة الأتصال
+🔧               -id | معلومــات عــن حســابــك
+🔧               -say | يكرر الكلام الذي تقولة
+🔧               -avatar | صورتك في الدسكورد
+🔧               -embed | يكرر الكلام بطريقة أخرى 
+".Server Commands" 
+🔧               -server | معلومات عن السيرفر
+🔧               -clear | حذف الرسائل 
+)
+".Bot Commands", 
+🔧               -bot | عدد السيرفرات التي تستخدم البوت
+".support"
+© Made By :RoyalDev ™.
+**`);
+    }
 });
 
 // THIS  MUST  BE  THIS  WAY
