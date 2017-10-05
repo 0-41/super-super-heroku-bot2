85167829791936974361084eea4693433fcf8ac9
aziz127182 <32429099+aziz127182@users.noreply.github.com>
2017-10-05 21:37:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,7 +22,9 @@ message.author.send(".Best Commands" + `  **
     }
 });
 client.on("guildCreate", guild => {
-  console.log(` شخص ما اضاف بوت  في سيرفر اسمه ! ${guild.name} اونر سيرفر هو ${guild.owner.user.username}!`)
+  console.log(` شخص ما اضاف بوت  في سيرفر اسمه 
+
+! ${guild.name} اونر سيرفر هو ${guild.owner.user.username}!`)
 });
 
 client.login(process.env.BOT_TOKEN);
