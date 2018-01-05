abc214ece20eeec614c7ed7171512a4a4e5457a4
aziz127182 <32429099+aziz127182@users.noreply.github.com>
2018-01-05 13:09:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,20 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag} !`);
-  client.user.setGame(`Hi Im Streaming !`,`www.twitch.tv/v5bz`);
+
+x5bz.on('message', message => {
+   let embed = new Discord.RichEmbed()
+    let args = message.content.split(' ').slice(1).join(' ');
+if(message.content.split(' ')[0] == '#bc') {
+        message.guild.members.forEach(m => {
+   if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('** You Dont Have `ADMINISTRATOR` Permission **');
+            var bc = new Discord.RichEmbed()
+            .setAuthor(`5bz`, 'https://images-ext-2.discordapp.net/external/4F4w1zP0y3ibykdGhcuAC9qTYPJzUHET_9VY6lBEj0M/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/293074606166769666/979a490c4c7ef5d320875cb51e16b462.png?width=201&height=201')
+            .addField('Server:', `${message.guild.name}`)
+            .addField('By:', `${message.author.username}#${message.author.discriminator}`)
+            .setColor('RANDOM')
+            .addField('Message: ', args)
+            m.send(`${m}`,{embed: bc});
+        });
+    }
 });
 client.login(process.env.BOT_TOKEN);
