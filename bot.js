881f9070a24ea895438357f29ff70c7fe0ceabc3
aziz127182 <32429099+aziz127182@users.noreply.github.com>
2018-01-05 13:39:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,22 +3,15 @@ const x5bz = new Discord.Client();
 x5bz.on('ready', () => {
   x5bz.user.setGame(`By: iiPixelSA`,'https://www.twitch.tv/v5bz');
   console.log('Im Ready!');
-});
-/* YT: iiPixlSA */
 x5bz.on('message', message => {
-   let embed = new Discord.RichEmbed()
-    let args = message.content.split(' ').slice(1).join(' ');
-if(message.content.split(' ')[0] == '#bc') {
-        message.guild.members.forEach(m => {
-   if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('** You Dont Have `ADMINISTRATOR` Permission **');
-            var bc = new Discord.RichEmbed()
-            .setAuthor(`5bz`, 'https://images-ext-2.discordapp.net/external/4F4w1zP0y3ibykdGhcuAC9qTYPJzUHET_9VY6lBEj0M/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/293074606166769666/979a490c4c7ef5d320875cb51e16b462.png?width=201&height=201')
-            .addField('Server:', `${message.guild.name}`)
-            .addField('By:', `${message.author.username}#${message.author.discriminator}`)
-            .setColor('RANDOM')
-            .addField('Message: ', args)
-            m.send(`${m}`,{embed: bc});
-        });
-    }
-});
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('!bc')){
+ if(!message.author.id === '256055323167555595') return;
+message.channel.sendMessage('تم , جار أرسال الرسالة')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+})
+
 x5bz.login(process.env.BOT_TOKEN);
