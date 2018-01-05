b46434cc5eca161ce96a536d4afd194493a45be0
aziz127182 <32429099+aziz127182@users.noreply.github.com>
2018-01-05 13:06:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,14 +4,4 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag} !`);
   client.user.setGame(`Hi Im Streaming !`,`www.twitch.tv/v5bz`);
 });
-clinet.on('message', message => {
-if (message.content.split(' ')[0] == '-bc')
- message.guild.members.forEach( member => {
- member.send(message.content.substr(3));
-});
-if (message.content.split(' ')[0] == '-broadcast')
-message.guild.members.forEach( member => {
-member.send(message.content.substr(10));
-});
-});
 client.login(process.env.BOT_TOKEN);
