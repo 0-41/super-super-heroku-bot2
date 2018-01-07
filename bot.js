5616f187a65bda62263c8c19e569a9081e6a4b0a
aziz127182 <32429099+aziz127182@users.noreply.github.com>
2018-01-07 17:19:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -434,23 +434,5 @@ x5bz.on('message', message => {
 
     }
 });
-x5bz.on('message', message => {
- if (message.content.includes('discord.gg')){
-                     if(!message.channel.guild) return message.reply ('')
-                 if (!message.member.hasPermissions(['MANAGE_MESSAGES'])){
-    message.channel.send('kick <@' + message.author.id + '>')
-    message.delete() 
-    }
- }
-       if (message.content.startsWith("kick ")) {
-          if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply();
-          var member= message.mentions.members.first();
-          member.kick().then((member) => {
-              message.channel.sendMessage("", {embed: {
-              author: {
-              },
-              title: 'بسبب النشر ' + member.displayName + ' تم طرد',
-              color: 490101,
-             }
-});
+
 x5bz.login(process.env.BOT_TOKEN);
