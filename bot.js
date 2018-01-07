4a5ccd94351f71f775856eb4f5777f54c9d62cce
aziz127182 <32429099+aziz127182@users.noreply.github.com>
2018-01-07 19:28:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -573,17 +573,16 @@ x5bz.on('message', message => {
               });
                 }
 	});
-var prefix = "."
 x5bz.on('message', message => {
   if (message.author.x5bz) return;
-  if (!message.content.startsWith(prefix)) return;
+  if (!message.content.startsWith()) return;
 
   let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
+  command = command.slice(.length);
 
   let args = message.content.split(" ").slice(1);
 
-  if (command == "kick") {
+  if (command == "اطرده بالله") {
                if(!message.channel.guild) return message.reply('** This command only for servers**');
          
   if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return message.reply("**You Don't Have ` KICK_MEMBERS ` Permission**");
