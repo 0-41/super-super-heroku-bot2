fdf6cf89f3b3b5c2ddfe2df4d17a53e644805aa7
aziz127182 <32429099+aziz127182@users.noreply.github.com>
2018-01-08 03:28:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -630,7 +630,7 @@ x5bz.on("message", (message) => {
 if (message.content.startsWith("f!cr")) {
             if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply("You Don't Have `MANAGE_CHANNELS` Premissions ");
         let args = message.content.split(" ").slice(1);
-    message.guild.createChannel(args.join(' '), 'role');
+    message.guild.serversetting(args.join(' '), 'roles');
     message.channel.sendMessage('تـم إنـشاء رتبه')
 }
 });
