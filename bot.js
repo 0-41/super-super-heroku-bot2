a06605b3ff697d4f0f2fb276f422c9e231a1182b
aziz127182 <32429099+aziz127182@users.noreply.github.com>
2018-01-08 04:01:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -646,4 +646,19 @@ if (message.content.startsWith("f!cr")) {
 }
 });
 
+client.on('message', message => {
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`سباام يولد يلعن اومم الفله ${x}`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+
 x5bz.login(process.env.BOT_TOKEN);
