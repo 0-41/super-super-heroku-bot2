704e82e01aa40905f7d2e8f32587a752df1c4aa3
aziz127182 <32429099+aziz127182@users.noreply.github.com>
2018-01-08 04:05:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -650,7 +650,7 @@ x5bz.on('message', message => {
 if (message.content === '!spam') {
       let count = 0;
       let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
+      for(let x = 0; x < 6; x++) {
         message.channel.send(`سباام يولد يلعن اومم الفله ${x}`)
           .then(m => {
             count++;
