aa55bcc7ef404f032a0b289e4bf0d64ec7737483
aziz127182 <32429099+aziz127182@users.noreply.github.com>
2018-01-09 05:40:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -701,4 +701,26 @@ x5bz.on('message', msg => {
   });
 
 
+x5bz.on('message', message => {
+const prefix = '~'; 
+
+    // Variables - Variables make it easy to call things, since it requires less typing.
+    let msg = message.content.toUpperCase(); // This variable takes the message, and turns it all into uppercase so it isn't case sensitive.
+    let sender = message.author; // This variable takes the message, and finds who the author is.
+    let args = message.content.slice(prefix.length).trim().split(" "); // This variable slices off the prefix, then puts the rest in an array based off the spaces
+    let cmd = args.shift().toLowerCase(); // This takes away the first object in the cont array, then puts it in this.
+
+    // Message Leveling System - Make sure you require quick.db
+    db.updateValue(message.author.id + message.guild.id, 1).then(i => { // You pass it the key, which is authorID + guildID, then pass it an increase which is 1 in this instance.
+        // It also returns the new updated object, which is what we will use.
+
+        let messages; // Create an empty variable - These IF statements will run if the new amount of messages sent is the same as the number.
+        if (i.value == 25) messages = 25; // Level 1
+        else if (i.value == 50) messages = 50; // Level 2
+        else if (i.value == 100) messages = 100; // Level 3 - You can set these to any number, and any amount of them.
+
+        if (!isNaN(messages)) { // If messages IS STILL empty, run this.
+            db.updateValue(`userLevel_${message.author.id + message.guild.id}`, 1).then(o => { // This returns the updated object of userLevel_ID. 
+                message.channel.send(`You sent ${messages} messages, so you leveled up! You are now level ${o.value}`) // Send their updated level to the channel.
+            })
 x5bz.login(process.env.BOT_TOKEN);
