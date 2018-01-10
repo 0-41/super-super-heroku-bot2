819fcaa2894221e47ab51fa49ad8c351467d3152
aziz127182 <32429099+aziz127182@users.noreply.github.com>
2018-01-10 20:10:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -800,24 +800,13 @@ x5bz.on('message', message => {
 
     }
 });
+
 x5bz.on('message', message => {
     var args = message.content.split(/[ ]+/)
-    if(message.content.includes('https://www.youtube.com')){
+    if(message.content.includes('www.youtube.com')){
         message.delete()
     return message.reply(`** No Invite Links :angry: ! **`)
-    member.addRole(admin);
-	        message.channel.send('${Member} has been added to PUGS');
-
-    }
-});
-x5bz.on('message', message => {
-    var args = message.content.split(/[ ]+/)
-    if(message.content.includes('https://youtu.be/')){
-        message.delete()
-    return message.reply(`** No Invite Links :angry: ! **`)
-    member.addRole(admin);
-	        message.channel.send('${Member} has been added to PUGS');
-
+x5bz.channels.get("306456660225032192")
     }
 });
 x5bz.login(process.env.BOT_TOKEN);
