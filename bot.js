f78069ac91b0f771c72827c8251b3d8b210ad453
aziz127182 <32429099+aziz127182@users.noreply.github.com>
2018-01-17 06:54:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,14 @@
 const Discord = require('discord.js');
-const x5bz = new Discord.Client();
-
-x5bz.on('message', function(message) {
-    if(message.content.startsWith(='support')) {
+const clinet = new Discord.Client();
+var prefix = "^"
+client.on('message', function(message) {
+    if(message.content.startsWith(prefix + 'support')) {
         let guild = message.mentions.members.first();
-                          let ZmA = new x5bz.RichEmbed()
+                          let ZmA = new Discord.RichEmbed()
                   .setColor('3fcf24')
                   .setDescription('**__:white_check_mark: تم إضافة رتبة الدعم الفني لك__**')
         message.member.addRole(message.guild.roles.find('name', 'Support'));
                     message.channel.send({embed:ZmA});
     }
 });
-x5bz.login(process.env.BOT_TOKEN);
+clinet.login(process.env.BOT_TOKEN);
