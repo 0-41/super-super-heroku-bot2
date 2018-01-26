63940cebeeff0b86fe5c5534330057263622ed55
aziz127182 <32429099+aziz127182@users.noreply.github.com>
2018-01-26 10:14:43 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -818,16 +818,24 @@ x5bz.on('message', message => {
                     message.channel.send({embed:ZmA});
     }
 });
-var x1 = ".رتبة"
-x5bz.on('message', message => {
-if (message.content === x1) {
-message.member.addRole(message.guild.roles.find("name", "⌐ admin"));
-const embed = new Discord.RichEmbed()
-.setThumbnail(message.author.avatarURL)
-.addField('Requested by:', "<@" + message.author.id + ">")
-.setDescription(`تم`)
-message.channel.sendEmbed(embed).then(message => {message.delete(3000)})
-    }
+x5bz.on('message', message => { 
+var prefix = ".";
+if (message.author.boss) return;
+if (!message.content.startsWith(prefix)) return;
+let command = message.content.split(" ")[0];
+command = command.slice(prefix.length);
+if (command == "Roleadd") {
+if (!message.channel.guild) return;
+if(!message.guild.member(message.author).hasPermission("MANAGE_ROLES")) return message.reply("**:no_entry_sign:انت لا تملك صلاحيات **").then(msg => msg.delete(5000));;
+if(!message.guild.member(x5bz.user).hasPermission("MANAGE_ROLES")) return message.reply("البوت لايملك صلاحيات ").then(msg => msg.delete(5000));;
+let user = message.mentions.users.first();
+if (message.mentions.users.size < 1) return message.reply('**ضع منشن الشخص!!**').then(msg => {msg.delete(5000)});
+let MRole = message.content.split(" ").slice(2).join(" ");
+if(!MRole)return message.reply("يجب عليك وضع اسم الرتبة").then(msg => {msg.delete(5000)});
+message.guild.member(user).addRole(message.guild.roles.find("name", MRole));
+message.reply('*** Done :white_check_mark:  ***').then(msg => {msg.delete(10000)});
+}
 });
+
 x5bz.login(process.env.BOT_TOKEN);
 
