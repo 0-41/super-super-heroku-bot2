b4aaa0836184bc564e9cd176615f56d8d9799d6e
aziz127182 <32429099+aziz127182@users.noreply.github.com>
2018-01-27 22:30:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
  
-var x2 = "رتبة ساموراي"
-client.on('message', message => {
-if (message.content === x2) {
-message.member.addRole(message.guild.roles.find("name", "►سہامورايہ◄"));
-const embed = new Discord.RichEmbed()
-.setThumbnail(message.author.avatarURL)
-.addField('Requested by:', "<@" + message.author.id + ">")
-.setDescription(`تم`)
-message.channel.sendEmbed(embed).then(message => {message.delete(3000)})
-    }
-});
 var x3 = "رتبة شرير"
 client.on('message', message => {
 if (message.content === x3) {
