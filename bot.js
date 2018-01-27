a39da4aa554e8f56b4ff69f50ce734ba748c7740
aziz127182 <32429099+aziz127182@users.noreply.github.com>
2018-01-27 22:36:47 +0300

    ►بہطہل خہأرقہ◄

--- bot.js
+++ bot.js
@@ -24,7 +24,17 @@ const embed = new Discord.RichEmbed()
 message.channel.sendEmbed(embed).then(message => {message.delete(3000)})
     }
 });
-
+var x5 = "رتبة بطل خارق"
+client.on('message', message => {
+if (message.content === x5) {
+message.member.addRole(message.guild.roles.find("name", "►بہطہل خہأرقہ◄"));
+const embed = new Discord.RichEmbed()
+.setThumbnail(message.author.avatarURL)
+.addField('Requested by:', "<@" + message.author.id + ">")
+.setDescription(`تم`)
+message.channel.sendEmbed(embed).then(message => {message.delete(3000)})
+    }
+});
 
 client.login(process.env.BOT_TOKEN);
 
