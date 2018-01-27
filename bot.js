571420b33cfd47a6cce4ef87cd919175d0c21a41
aziz127182 <32429099+aziz127182@users.noreply.github.com>
2018-01-27 20:58:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -804,7 +804,7 @@ x5bz.on('message', message => {
 x5bz.channels.get("306456660225032192")
     }
 });
-client.on('message', message => { 
+x5bz.on('message', message => { 
 var prefix = ".";
 if (message.author.boss) return;
 if (!message.content.startsWith(prefix)) return;
@@ -813,7 +813,7 @@ command = command.slice(prefix.length);
 if (command == "Roleadd") {
 if (!message.channel.guild) return;
 if(!message.guild.member(message.author).hasPermission("MANAGE_ROLES")) return message.reply("**:no_entry_sign:انت لا تملك صلاحيات **").then(msg => msg.delete(5000));;
-if(!message.guild.member(client.user).hasPermission("MANAGE_ROLES")) return message.reply("البوت لايملك صلاحيات ").then(msg => msg.delete(5000));;
+if(!message.guild.member(x5bz.user).hasPermission("MANAGE_ROLES")) return message.reply("البوت لايملك صلاحيات ").then(msg => msg.delete(5000));;
 let user = message.mentions.users.first();
 if (message.mentions.users.size < 1) return message.reply('**ضع منشن الشخص!!**').then(msg => {msg.delete(5000)});
 let MRole = message.content.split(" ").slice(2).join(" ");
