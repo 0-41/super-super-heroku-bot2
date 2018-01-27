3643798082335cbbb3c152a13da9f2dc80cfcab4
aziz127182 <32429099+aziz127182@users.noreply.github.com>
2018-01-27 21:38:04 +0300

    Update bot.js
    ►شہريہر◄

--- bot.js
+++ bot.js
@@ -12,6 +12,17 @@ const embed = new Discord.RichEmbed()
 message.channel.sendEmbed(embed).then(message => {message.delete(3000)})
     }
 });
+var x1 = "رتبة شرير"
+client.on('message', message => {
+if (message.content === x1) {
+message.member.addRole(message.guild.roles.find("name", "►شہريہر◄"));
+const embed = new Discord.RichEmbed()
+.setThumbnail(message.author.avatarURL)
+.addField('Requested by:', "<@" + message.author.id + ">")
+.setDescription(`تم`)
+message.channel.sendEmbed(embed).then(message => {message.delete(3000)})
+    }
+});
 
 
 client.login(process.env.BOT_TOKEN);
