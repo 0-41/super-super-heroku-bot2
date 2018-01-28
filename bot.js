0885ad22ef5c7faf6c05d9144336f8f518f48c2b
aziz127182 <32429099+aziz127182@users.noreply.github.com>
2018-01-29 02:44:58 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -143,5 +143,27 @@ message.channel.sendEmbed(embed).then(message => {message.delete(3000)})
 
             }
 });
+
+client.on("message", (message) => {
+if (message.content.startsWith("f!ct")) {
+            if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply("You Don't Have `MANAGE_CHANNELS` Premissions ");
+        let args = message.content.split(" ").slice(1);
+    message.guild.createChannel(args.join(' '), 'text');
+message.channel.sendMessage('تـم إنـشاء روم كـتابـي')
+
+}
+});
+
+
+client.on("message", (message) => {
+if (message.content.startsWith("f!cv")) {
+            if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply("You Don't Have `MANAGE_CHANNELS` Premissions ");
+        let args = message.content.split(" ").slice(1);
+    message.guild.createChannel(args.join(' '), 'voice');
+    message.channel.sendMessage('تـم إنـشاء روم صـوتي')
+    
+}
+});
+
 client.login(process.env.BOT_TOKEN);
 
