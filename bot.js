b9a66bd2cb60a06f4ab4950dc5d9905414c247e4
aziz127182 <32429099+aziz127182@users.noreply.github.com>
2018-02-13 17:22:51 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const sql = require("sqlite");
+sql.open("./credits.sqlite");
  
 var x3 = "رتبة شرير"
 client.on('message', message => {
@@ -172,5 +174,47 @@ client.on('message',function(message) {
     message.reply("**ممنوع السب**"); 
   }
 });
+
+const prefix = "-";
+client.on("message", message => {
+  if (message.author.bot) return;
+  if (message.channel.type !== "text") return;
+
+  sql.get(`SELECT * FROM scores WHERE userId ="${message.author.id}"`).then(row => {
+    if (!row) {
+      sql.run("INSERT INTO scores (userId, credits,) VALUES (?, ?, ?)", [message.author.id, 1, 0]);
+    } else {
+      let curLevel = Math.floor(0.1 * Math.sqrt(row.points + 1));
+      if (curLevel > row.level) {
+        row.level = curLevel;
+        sql.run(`UPDATE scores SET points = ${row.points + 1}, level = ${row.level} WHERE userId = ${message.author.id}`);
+        message.reply(`You've leveled up to level **${curLevel}**! Ain't that dandy?`);
+      }
+      sql.run(`UPDATE scores SET points = ${row.points + 1} WHERE userId = ${message.author.id}`);
+    }
+  }).catch(() => {
+    console.error;
+    sql.run("CREATE TABLE IF NOT EXISTS scores (userId TEXT, points INTEGER, level INTEGER)").then(() => {
+      sql.run("INSERT INTO scores (userId, points, level) VALUES (?, ?, ?)", [message.author.id, 1, 0]);
+    });
+  });
+
+  if (!message.content.startsWith(prefix)) return;
+
+  if (message.content.startsWith(prefix + "SoOoN")) {
+    sql.get(`SELECT * FROM scores WHERE userId ="${message.author.id}"`).then(row => {
+      if (!row) return message.reply("Your current level is 0");
+      message.reply(`Your current level is ${row.level}`);
+    });
+  } else
+
+  if (message.content.startsWith(prefix + "credits")) {
+    sql.get(`SELECT * FROM scores WHERE userId ="${message.author.id}"`).then(row => {
+      if (!row) return message.reply('Sorry your :credit_card: balance is $0.');
+      message.reply( 'Your Balance is ' +  `${row.points}` + '$' + ' :credit_card:');
+    });
+  }
+});
+
 client.login(process.env.BOT_TOKEN);
 
