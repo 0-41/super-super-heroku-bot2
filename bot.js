7d16633cb35b0a07c5db6feb0c3f97c14d9980ac
aziz127182 <32429099+aziz127182@users.noreply.github.com>
2018-02-19 04:26:34 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on("message", (message) => {
-if (message.content.startsWith("سوي شات")) {
+if (message.content.startsWith("f!ct")) {
             if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply("You Don't Have `MANAGE_CHANNELS` Premissions ");
         let args = message.content.split(" ").slice(1);
     message.guild.createChannel(args.join(' '), 'text');
