66948e4b867bcb8a50830674216cd82ad2d0d2d6
aziz127182 <32429099+aziz127182@users.noreply.github.com>
2018-02-22 06:54:52 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,4 +18,32 @@ if (message.content === '!spam') {
       }
 });
 
+client.on('ready', () => {
+    client.user.setStatus('idle');
+});
+
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(`**
+      حياك الله ي بعد راسي
+       خش بتنورنا فعاليات وكل شيء حلو موجود !
+        ي بعد عيني الرابط تحت
+         اسم سيرفر OGS (:
+
+                                 [ https://discord.gg/NaSCNd ] **`)
+}).catch(console.error)
+})
+
+client.on("guildMemberRemove", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(`**
+      حياك الله ي بعد راسي
+       خش بتنورنا فعاليات وكل شيء حلو موجود !
+        ي بعد عيني الرابط تحت
+         OGS server (:
+
+                                 [ https://discord.gg/NaSCNd ] **`)
+}).catch(console.error)
+})
+
 client.login(process.env.BOT_TOKEN);
