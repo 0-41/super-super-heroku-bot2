83b7188e855b77c41c4ff70e335f27ce7508ce17
aziz127182 <32429099+aziz127182@users.noreply.github.com>
2018-02-22 07:44:40 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -45,5 +45,27 @@ client.on("guildMemberRemove", member => {
                                  [ https://discord.gg/NaSCNd ] **`)
 }).catch(console.error)
 })
-
+client.on("message", message => {
+    var prefix = "!";
+            var args = message.content.substring(prefix.length).split(" ");
+            if (message.content.startsWith(prefix + "clear")) {
+                if (!message.member.hasPermission("MANGE_MESSAGES"))  return;
+ if (!args[1]) {
+                                let embed3 = new Discord.RichEmbed()
+                                .setDescription(".clear <number>")
+                                .setColor("RANDOM")
+                                message.channel.sendEmbed(embed3);
+                            } else {
+                            let messagecount = parseInt(args[1]);
+                            message.channel.fetchMessages({limit: messagecount}).then(messages => message.channel.bulkDelete(messages));
+                                                          message.channel.fetchMessages({limit: messagecount}).then(messages => message.channel.bulkDelete(messages));
+                            message.channel.fetchMessages({limit: messagecount}).then(messages => message.channel.bulkDelete(messages));
+                            let embed4 = new Discord.RichEmbed()
+                                                            .setColor("#008000")
+                                .setDescription(":white_check_mark: | Delete " + args[1] + " Message!")
+                                                                                        message.delete("..");
+                                message.channel.sendEmbed(embed4);
+                            }
+                          }
+});
 client.login(process.env.BOT_TOKEN);
