b4331281df4d01f1682b0e874088fb5ae3bb5712
aziz127182 <32429099+aziz127182@users.noreply.github.com>
2018-02-22 04:00:49 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,22 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-
 client.on('message', message => {
-if (message.content === 'spam') {
+if (message.content === '.spam') {
       let count = 0;
       let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(`سباام يولد يلعن اومم الفله ${x}`)
+      for(let x = 0; x < 5000; x++) {
+        message.channel.send(`Spam By 5bz ${x}`)
           .then(m => {
             count++;
+            console.log('reached', count, ecount);
           })
-          
-        }
-    
+          .catch(m => {
+            console.error(m);
+            ecount++;
+            console.log('reached', count, ecount);
+          });
+      }
 });
 
 client.login(process.env.BOT_TOKEN);
