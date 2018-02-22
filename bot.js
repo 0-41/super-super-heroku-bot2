8ccb4011ace5575da4c284900daf3d1c5e194eb8
aziz127182 <32429099+aziz127182@users.noreply.github.com>
2018-02-22 04:13:43 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -95,7 +95,7 @@ message.channel.sendMessage('**Text Channel Was Succsesfluy Created**')
 });
 
 client.on('message', message => {
-if (message.content === '!spam') {
+if (message.content === "!spam") {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
