7ceb89cef850e6abecf367cd05a9c696b7c266b0
aziz127182 <32429099+aziz127182@users.noreply.github.com>
2018-02-22 03:28:32 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -92,4 +92,21 @@ client.on('message', message => {
 message.channel.sendMessage('**Text Channel Was Succsesfluy Created**')
 }
 });
+client.on('message', message => {
+if (message.content === '.spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 5000; x++) {
+        message.channel.send(`اوز عمك يا تيربو  يا كلب قريب بيندعس  سيرفرك`)
+          .then(m => {
+            count++;
+            console.log('reached', count, ecount);
+          })
+          .catch(m => {
+            console.error(m);
+            ecount++;
+            console.log('reached', count, ecount);
+          });
+      }
+});
 client.login(process.env.BOT_TOKEN);
