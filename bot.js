765581e65f406de2938ce0b24055911862c97a67
aziz127182 <32429099+aziz127182@users.noreply.github.com>
2018-02-22 04:10:58 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -94,4 +94,15 @@ message.channel.sendMessage('**Text Channel Was Succsesfluy Created**')
 }
 });
 
+client.on('message', message => {
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`سباام يولد يلعن اومم الفله ${x}`)
+          .then(m => {
+            count++;
+        
+      }
+});
 client.login(process.env.BOT_TOKEN);
